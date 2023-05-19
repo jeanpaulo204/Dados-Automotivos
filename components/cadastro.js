28c81f1c91fcc5a75d9be0c0be9709a585e86a55
JeanPaulo <jeanpaulo204@hotmail.com>
2023-05-19 00:05:15 -0300

    tela de login

---
+++ components/cadastro.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+
+const Cadastro = () => {
+  const [nome, setNome] = useState('');
+  const [veiculo, setVeiculo] = useState('');
+
+  const handleCadastro = () => {
+    // Aqui você pode adicionar a lógica para lidar com o cadastro dos dados
+    // Por exemplo, enviar os dados para um servidor ou atualizar o estado da aplicação
+
+    console.log('Nome:', nome);
+    console.log('Veículo:', veiculo);
+
+    // Limpar os campos após o cadastro
+    setNome('');
+    setVeiculo('');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Input
+        label="Nome"
+        placeholder="Digite seu nome"
+        value={nome}
+        onChangeText={setNome}
+        containerStyle={styles.inputContainer}
+        inputStyle={styles.input}
+        labelStyle={styles.label}
+      />
+      <Input
+        label="Veículo"
+        placeholder="Digite o tipo de veículo"
+        value={veiculo}
+        onChangeText={setVeiculo}
+        containerStyle={styles.inputContainer}
+        inputStyle={styles.input}
+        labelStyle={styles.label}
+      />
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    paddingTop: 80,
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  input: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+  },
+  label: {
+    fontSize: 14,
+    marginBottom: 4,
+  },
+  button: {
+    marginTop: 16,
+    backgroundColor: '#ff9800',
+  },
+});
+
+export default Cadastro;
---
+++ components/form.js
@@ -0,0 +1,224 @@
+import React, { useState } from 'react';
+import { View, FlatList, TouchableOpacity, StyleSheet, Alert, Text } from 'react-native';
+import { Input, Button, ListItem, Icon } from 'react-native-elements';
+import { format } from 'date-fns';
+
+const CrudExample = () => {
+  const [data, setData] = useState([]);
+  const [date, setDate] = useState(new Date());
+  const [km, setKm] = useState('');
+  const [gasolina, setGasolina] = useState('');
+  const [editingItemId, setEditingItemId] = useState(null);
+  const [showMessage, setShowMessage] = useState(false);
+  const [showEditMessage, setShowEditMessage] = useState(false);
+
+  const handleAddItem = () => {
+    if (km.trim() === '' || gasolina.trim() === '') {
+      return;
+    }
+
+    if (editingItemId) {
+      const updatedData = data.map((item) => {
+        if (item.id === editingItemId) {
+          return {
+            ...item,
+            date,
+            km,
+            gasolina,
+          };
+        }
+        return item;
+      });
+
+      setData(updatedData);
+      setShowEditMessage(true);
+      setTimeout(() => {
+        setShowEditMessage(false);
+      }, 1000);
+      setEditingItemId(null);
+    } else {
+      const newItem = {
+        id: Date.now().toString(),
+        date,
+        km,
+        gasolina,
+      };
+
+      setData([...data, newItem]);
+    }
+
+    setDate(new Date());
+    setKm('');
+    setGasolina('');
+  };
+
+  const handleDeleteItem = (id) => {
+    Alert.alert(
+      'Confirmação',
+      'Você tem certeza que deseja excluir este item?',
+      [
+        { text: 'Cancelar', style: 'cancel' },
+        {
+          text: 'Excluir',
+          style: 'destructive',
+          onPress: () => {
+            const newData = data.filter((item) => item.id !== id);
+            setData(newData);
+            setShowMessage(true);
+            setTimeout(() => {
+              setShowMessage(false);
+            }, 1000);
+          },
+        },
+      ],
+    );
+  };
+
+  const handleEditItem = (item) => {
+    setEditingItemId(item.id);
+    setDate(item.date);
+    setKm(item.km);
+    setGasolina(item.gasolina);
+  };
+
+  const formatDate = (date) => {
+    return format(date, 'dd/MM/yyyy');
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.itemContainer}>
+      <ListItem.Content>
+        <ListItem.Title>{formatDate(item.date)}</ListItem.Title>
+        <ListItem.Subtitle>{item.km} KM</ListItem.Subtitle>
+        <ListItem.Subtitle>{item.gasolina} Litros</ListItem.Subtitle>
+      </ListItem.Content>
+      <View style={styles.itemActions}>
+        <Icon
+          name="pencil"
+          type="material-community"
+          color="#9e9e9e"
+          onPress={() => handleEditItem(item)}
+        />
+        <Icon
+          name="delete"
+          type="material"
+          color="#ff5252"
+          onPress={() => handleDeleteItem(item.id)}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Icon name="car" type="font-awesome" size={40} color="#fff" />
+      </View>
+
+      <View style={styles.formContainer}>
+        <Input
+          label="Data"
+          placeholder="Digite a data"
+          value={formatDate(date)}
+          editable={false}
+        />
+        <Input
+          label="Quantos KM?"
+          placeholder="Digite a quantidade de KM"
+          keyboardType="numeric"
+          value={km}
+          onChangeText={(text) => setKm(text)}
+        />
+        <Input
+          label="Gasolina (Litros)"
+          placeholder="Digite a quantidade de gasolina"
+          keyboardType="numeric"
+          value={gasolina}
+          onChangeText={(text) => setGasolina(text)}
+        />
+        <Button
+          title={editingItemId ? 'Salvar' : 'Adicionar'}
+          onPress={handleAddItem}
+          disabled={km.trim() === '' || gasolina.trim() === ''}
+          buttonStyle={styles.addButton}
+        />
+      </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContainer}
+      />
+
+      {showMessage && (
+        <View style={styles.messageContainer}>
+          <Text style={styles.messageText}>Foi excluído com sucesso!</Text>
+        </View>
+      )}
+
+      {showEditMessage && (
+        <View style={styles.messageContainer}>
+          <Text style={styles.messageText}>Foi editado com sucesso!</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00bcd4',
+    paddingHorizontal: 16,
+    paddingTop: 80,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 16,
+  },
+  formContainer: {
+    marginBottom: 16,
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+    elevation: 3,
+  },
+  addButton: {
+    backgroundColor: '#ff9800',
+  },
+  listContainer: {
+    flexGrow: 1,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 16,
+    marginBottom: 8,
+    borderRadius: 8,
+    elevation: 2,
+  },
+  itemActions: {
+    flexDirection: 'row',
+  },
+  messageContainer: {
+    position: 'absolute',
+    bottom: 16,
+    left: 16,
+    right: 16,
+    backgroundColor: '#51B449',
+    padding: 16,
+    borderRadius: 8,
+    elevation: 3,
+  },
+  messageText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default CrudExample;
